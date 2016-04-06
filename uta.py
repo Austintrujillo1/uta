58e8962a8312b6af25e7fa8a910b96fe8b574f50
Austin Trujillo <Austintrujillo1@me.com>
2016-04-06 11:39:08 -0600

    Successfull pulling and printing of line information. TODO: database of Stop names with their NB and SB IDs.

--- uta.py
+++ uta.py
@@ -1,11 +1,13 @@
 import requests
-import xml.etree.ElementTree as etree
+import xmldict
+
+from clint.textui import colored, puts
 
 api_key = "UPIIJBD0QEG"
 
 url = "http://api.rideuta.com/SIRI/SIRI.svc/"
 url += "StopMonitor?stopid=" #Query type
-url += "TX136084" #Stop ID
+url += "TX153041" #Stop ID
 url += "&minutesout=30" #How far in the future to query
 url += "&onwardcalls=false" #Include vehicle calls inbetween current location and stop
 url += "&filterroute=" #Filter vehicles
@@ -15,7 +17,47 @@ url += api_key
 r = requests.get(url)
 xml = r.content
 
-root = etree.fromstring(xml)
+d = xmldict.xml_to_dict(xml)
+d2 = d['{http://www.siri.org.uk/siri}Siri']['{http://www.siri.org.uk/siri}StopMonitoringDelivery']['{http://www.siri.org.uk/siri}MonitoredStopVisit']['{http://www.siri.org.uk/siri}MonitoredVehicleJourney']
+print "\n"
+print "QUERY STATION: ", d['{http://www.siri.org.uk/siri}Siri']['{http://www.siri.org.uk/siri}StopMonitoringDelivery']['{http://www.siri.org.uk/siri}Extensions']['{http://www.siri.org.uk/siri}StopName']
+print "\n"
+for x in d2:
 
-for child in root[1][2]:
-   print(child.tag, child.attrib)
+    xname = str(x['{http://www.siri.org.uk/siri}PublishedLineName'])
+    xlinenumber = str(x['{http://www.siri.org.uk/siri}LineRef'])
+    xdest = str(x['{http://www.siri.org.uk/siri}DirectionRef'])
+
+    if xname == "RED LINE":
+
+        if xdest == 'TO DAYBREAK':
+            xdest = "SB"
+        else:
+            xdest == "NB"
+
+        puts(colored.red("Line Name: %s" % xname))
+        puts(colored.red("Line Number: %s" % xlinenumber))
+        puts(colored.red("Direction: %s" % xdest))
+        print "\n"
+    elif xname == "GREEN LINE":
+
+        if xdest == 'TO WEST VALLEY':
+            xdest = "SB"
+        else:
+            xdest == "NB"
+
+        puts(colored.green("Line Name: %s" % xname))
+        puts(colored.green("Line Number: %s" % xlinenumber))
+        puts(colored.green("Direction: %s" % xdest))
+        print "\n"
+    elif xname == "BLUE LINE":
+
+        if xdest == 'TO DRAPER':
+            xdest = "SB"
+        else:
+            xdest == "NB"
+
+        puts(colored.blue("Line Name: %s" % xname))
+        puts(colored.blue("Line Number: %s" % xlinenumber))
+        puts(colored.blue("Direction: %s" % xdest))
+        print "\n"
